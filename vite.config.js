2d8b7875f3a3ebbb452348796f22c469c9811102
Ankit Gade <ankit.gade@10up.com>
2024-12-01 10:10:18 +0530

    Add rollupOptions to vite.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  rollupInputOptions: {
-    external: ['typed.js'],
-  }
+  build: {
+    rollupOptions: {
+      input: {
+        index: './index.html', // Default entry point
+        services: './services.html',
+        privacyPolicy: './privacy-policy.html',
+        terms: './terms.html',
+        refundPolicy: './refund-policy.html',
+      },
+      external: ['typed.js'], // Exclude specific modules from bundling
+    },
+  },
 });
